4cbe4ce7906b07bbd6fd27cc81b007b25a32e029
Mituls-code <mitulpatel9897@gmail.com>
2021-05-24 12:10:33 +0530

    update

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -1,12 +1,17 @@
-let sub = ["html", "java", "css", "#c", "javaScript"];
+let sub = prompt("Enter total subject : ");
+let subject = [];
 let tot = 0;
 let txt = "<table><tr><th>Subject</th><th>Marks</th></tr>"
-
-for (let i = 0; i < sub.length; i++) {
-    let marks = prompt("Enter mark of " + sub[i]);
-    tot += parseInt(marks);
-    txt += "<tr> <td>" + sub[i] + "</td><td>" + marks + "</td ></tr>";
+for (i = 0; i < sub; i++) {
+    subject[i] = prompt("Enter subject Name : ");
 }
 
-txt += "<tr><td>Total</td><td>" + tot + "</td><tr><td>Percentage</td><td>" + tot / sub.length + "</td></table>";
+for (let i = 0; i < subject.length; i++) {
+
+    let marks = prompt("Enter mark of " + subject[i]);
+    tot += parseInt(marks);
+    txt += "<tr> <td>" + subject[i] + "</td><td>" + marks + "</td ></tr>";
+}
+
+txt += "<tr><td>Total</td><td>" + tot + "</td><tr><td>Percentage</td><td>" + tot / subject.length + "</td></table>";
 document.write(txt);
